90194d1e7716f6341f45586d2186fcda7167fab1
Blake Pozolo <btpozolo@umich.edu>
2025-06-25 11:33:56 -0700

    Initial Commit

---
+++ NounsOnAPhone/NounsOnAPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NounsOnAPhoneApp.swift
+//  NounsOnAPhone
+//
+//  Created by Blake Pozolo on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct NounsOnAPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NounsOnAPhoneTests/NounsOnAPhoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  NounsOnAPhoneTests.swift
+//  NounsOnAPhoneTests
+//
+//  Created by Blake Pozolo on 6/25/25.
+//
+
+import Testing
+@testable import NounsOnAPhone
+
+struct NounsOnAPhoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
